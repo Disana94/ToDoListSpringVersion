e61dfc107a860d23ab666fa3830a040ef678ab0e
Sani94 <Dkhavzhokova@mail.ri>
2025-11-23 16:02:51 +0300

    init commit

---
+++ src/main/java/todolistspingversion/test/ToDoListSpingVersionApplication.java
@@ -0,0 +1,13 @@
+package todolistspingversion.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListSpingVersionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListSpingVersionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/todolistspingversion/test/ToDoListSpingVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package todolistspingversion.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListSpingVersionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
